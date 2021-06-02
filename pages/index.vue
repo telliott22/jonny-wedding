a11eeb641fd6916eb0935e8823567352fb901fab
Tim Elliott <telliott22@gmail.com>
2021-06-02 13:04:05 +0200

    :construction: basic content on

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,91 @@
 <template>
-  <div class="container mx-lg flex flex-col p-18 justify-center">
+  <div class="container mx-lg flex flex-col p-18 justify-center text-white max">
     <img
       src="/header-image.png"
       alt="Saturday 4th September"
       class="m-auto w-1/2 max-w-md mb-10"
     />
 
-    <div class="text-center">
-      <h1 class="text-xl text-white">Jonny and Tara's</h1>
+    <div class="max-w-2xl text-center m-auto">
+      <h1 class="text-xl text-white">Jonny and Tara's wedding</h1>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea,
-        consequuntur aperiam excepturi sequi inventore saepe exercitationem
-        error, doloremque illum impedit labore magni distinctio, enim totam
-        consectetur molestias laboriosam laudantium consequatur!
-      </p>
+      <div class="mb-10">
+        <h2 class="text-lg">Ceremony</h2>
+        <p>Ceremony starts at 1pm, please be seated no later than 12.30pm</p>
+      </div>
+
+      <div class="mb-10">
+        <h2 class="text-lg">Travel</h2>
+        <p>
+          The closest station to the Church is Kings Langley. Parking is
+          available. A bus will be provided to transport you from church to
+          venue.
+        </p>
+      </div>
+
+      <h2 class="text-lg mb-2">Accomodation</h2>
+
+      <div class="mb-6">
+        <h3 class="text-base">Near The Church</h3>
+        <ul>
+          <li>Premier Inn - Kings Langley</li>
+          <li>Holiday Inn - Watford Junction</li>
+          <li>Jurys Inn - Watford</li>
+        </ul>
+      </div>
+
+      <div class="mb-10">
+        <h3 class="text-base">Near The Reception Venue</h3>
+        <ul>
+          <li>Premier Inn - Hitchin Town Centre</li>
+          <li>Hilton Garden Inn</li>
+          <li>La Bella Vita</li>
+        </ul>
+      </div>
+
+      <div class="mb-10">
+        <h2 class="text-lg">Gifts</h2>
+        <p>
+          Whilst your presence is the best present we can ask for, if you wish
+          to give us a figt we would be gratfeul for any contribution to our
+          honeymoon. To contribute online, click
+          <a class="underline" href="#">here</a>
+        </p>
+      </div>
+
+      <div class="mb-10">
+        <h2 class="text-lg">Dresscode</h2>
+        <p>Jacket & Tie/Cocktail Attire</p>
+      </div>
+
+      <hr class="m-10" />
+
+      <h2 class="text-lg">Location</h2>
+
+      <a
+        class="mb-8 underline"
+        href="https://goo.gl/maps/gThMpzn3ij4nrZneA"
+        target="_blank"
+        >St Peter's Langleybury, Langleybury Lane, Hunton Bridge, WD4 8QQ
+      </a>
+
+      <div class="flex justify-center">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.160759710189!2d-0.4402947482802067!3d51.69350317956626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487641cfe4af61cb%3A0x8e611e462d8b5210!2sSt.%20Paul%E2%80%99s%20Church!5e0!3m2!1sen!2sde!4v1622628940046!5m2!1sen!2sde"
+          width="600"
+          height="450"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+
+      <hr class="m-10" />
+
+      <div class="mb-10">
+        <h2 class="text-lg">Ceremony</h2>
+        <p>Ceremony starts at 1pm, please be seated no later than 12.30pm</p>
+      </div>
     </div>
   </div>
 </template>
