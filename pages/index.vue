ea0af5d8870b662d0726e30a1080779a67ddea27
Tim Elliott <telliott22@gmail.com>
2021-06-06 11:17:50 +0200

    :construction: wedding reception details

--- pages/index.vue
+++ pages/index.vue
@@ -131,9 +131,7 @@
         </p>
       </div>
 
-      <hr class="m-10" />
-
-      <h2 class="text-lg">Location</h2>
+      <h2 class="text-lg">Wedding Location</h2>
 
       <a
         class="mb-8 underline cursor-pointer"
@@ -142,9 +140,56 @@
         >St Peter's Langleybury, Langleybury Lane, Hunton Bridge, WD4 8QQ
       </a>
 
-      <div class="flex justify-center">
+      <div class="flex justify-center mb-10">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.160759710189!2d-0.4402947482802067!3d51.69350317956626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487641cfe4af61cb%3A0x8e611e462d8b5210!2sSt.%20Paul%E2%80%99s%20Church!5e0!3m2!1sen!2sde!4v1622628940046!5m2!1sen!2sde"
+          width="600"
+          height="450"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+
+      <hr class="m-10" />
+
+      <div class="mb-10">
+        <h2 class="text-lg">Wedding Reception</h2>
+        <p>Wedding reception starts at 7.30pm.</p>
+      </div>
+
+      <div class="mb-10">
+        <h2 class="text-lg">RSVP</h2>
+
+        <p>
+          Please let us know if you can come to the wedding reception by July
+          1st 2021.
+        </p>
+        <p>
+          To RSVP for the evening event, click
+          <a
+            class="underline"
+            target="_blank"
+            href="https://forms.gle/RkP3TSW3f2XHF4ENA"
+            >here</a
+          >
+        </p>
+      </div>
+
+      <h2 class="text-lg">Evening Event Location</h2>
+
+      <a
+        class="mb-8 underline cursor-pointer"
+        href="https://goo.gl/maps/Qttr7DS7dRmfhz8v5"
+        target="_blank"
+      >
+        Offley Place Country House Hotel, Kings Walden Road, Great Offley,
+        Hitchin SG5 3DX
+      </a>
+
+      <div class="flex justify-center">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2460.239660196647!2d-0.3378226843965326!3d51.92958197970672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876367da3d44447%3A0x9d22d37dbc7ad0e8!2sOffley%20Place%20Country%20House%20Hotel!5e0!3m2!1sen!2sde!4v1622970774073!5m2!1sen!2sde"
           width="600"
           height="450"
           style="border: 0"
